e17b464323bc55044f4adae97e88bb2466345362
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-03-07 22:50:15 +0300

    complete lab changes

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 // Code your solution in this file!
 const companyName='Scuber';
 console.log("companyName");
+
+let mostProfitableNeighborhood = "Chelsea";
+console.log("mostProfitableNeighborhood");
+companyCeo = "Susan Smith";
+console.log("companyCeo");
